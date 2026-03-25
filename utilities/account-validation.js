407ce92588d94b6ca3029139f78a6ce9fc1b4aa0
NephiImo <imonephi@gmail.com>
2026-03-25 16:45:49 +0100

    Implement login functionality with validation; add bcrypt for password hashing and enhance error handling for registration and login views

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,7 +1,7 @@
 const utilities = require(".")
 const { body, validationResult } = require("express-validator")
 const validate = {}
-
+const accountModel = require("../models/account-model")
 
 /*  **********************************
   *  Registration Data Validation Rules
@@ -24,14 +24,18 @@ const validate = {}
         .isLength({ min: 2 })
         .withMessage("Please provide a last name."), // on error this message is sent.
   
-      // valid email is required and cannot already exist in the DB
-      body("account_email")
-      .trim()
-      .escape()
-      .notEmpty()
-      .isEmail()
-      .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+    // valid email is required and cannot already exist in the database
+    body("account_email")
+    .trim()
+    .isEmail()
+    .normalizeEmail() // refer to validator.js docs
+    .withMessage("A valid email is required.")
+    .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        if (emailExists){
+        throw new Error("Email exists. Please log in or use different email")
+        }
+    }),
   
       // password is required and must be strong password
       body("account_password")
@@ -70,5 +74,48 @@ validate.checkRegData = async (req, res, next) => {
   }
   next()
 }
+
+/*  **********************************
+ *  Login Data Validation Rules
+ * ********************************* */
+validate.loginRules = () => {
+    return [
+        // valid email is required
+        body("account_email")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isEmail()
+            .normalizeEmail() // refer to validator.js docs
+            .withMessage("A valid email is required."),
+
+        // password is required
+        body("account_password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required."),
+    ]
+}
+
+/* ******************************
+ * Check login data and return errors or continue to login process
+ * ***************************** */ 
+validate.checkLoginData = async (req, res, next) => {
+  const { account_email } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      errors,
+      title: "Login",
+      nav,
+      account_email,
+    })
+    return
+  }
+  next()
+}
+
 
 module.exports = validate
